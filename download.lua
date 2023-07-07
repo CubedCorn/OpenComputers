bbc0b6d1f361be92b4a2c7771487ff6e065c9f2b
CubedCorn <57824049+CubedCorn@users.noreply.github.com>
2023-07-07 18:13:04 -0500

    Add files via upload

---
+++ download.lua
@@ -0,0 +1,11 @@
+local shell = require('shell')
+local internet = component.internet
+local program = shell.parse(...)[1]
+local link = string.format('https://raw.githubusercontent.com/CubedCorn/OpenComputers/main/%s.lua', program)
+
+local data = internet.request(link)() -- request returns a response function
+if not data then print('program not found, please try again with a valid program.') end
+
+local file = io.open(program:lower(), 'w')
+file:write(data)
+file:close()
