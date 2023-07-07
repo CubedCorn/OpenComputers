0c5c2fcb2ad9acd7d1baa95ee864a58cb81b240b
CubedCorn <57824049+CubedCorn@users.noreply.github.com>
2023-07-07 18:21:14 -0500

    Update download.lua
    forgot to kill program if data is nil

--- download.lua
+++ download.lua
@@ -4,9 +4,10 @@ local program = shell.parse(...)[1]
 local link = string.format('https://raw.githubusercontent.com/CubedCorn/OpenComputers/main/%s.lua', program)
 
 local data = internet.request(link)() -- request returns a response function
-print(data)
-return
-if not data then print('program not found, please try again with a valid program.') end
+if not data then 
+  print('program not found, please try again with a valid program.') 
+  return
+end
 
 local file = io.open(program:lower(), 'w')
 file:write(data)
