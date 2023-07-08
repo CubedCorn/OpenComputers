b31bbf31c99c540d963e748a78bd059e0d93332f
CubedCorn <57824049+CubedCorn@users.noreply.github.com>
2023-07-07 19:47:08 -0500

    Update download.lua

--- download.lua
+++ download.lua
@@ -1,23 +1,28 @@
 local shell = require('shell')
 local internet = component.internet
-local program, options = shell.parse(...)[1]
+local program, options = shell.parse(...); program = program[1]
 local link = string.format('https://raw.githubusercontent.com/CubedCorn/OpenComputers/main/%s.lua', program)
 
 local timeout = computer.uptime() + 5
 if options[1] and string.sub(options[1], 7):lower() == 'timeout' then
-    timeout = computer.uptime() + tonumber(string.sub(options[1], 8, 10))
+    timeout = computer.uptime() + tonumber(string.sub(options[1], 8, 9))
 end
 
 local response = internet.request(link) -- request returns a response function
-    while response() == nil and computer.uptime() < timeout do
-        os.sleep()
+    while response() == nil do
+        if computer.uptime() < timeout then
+            os.sleep()
+        else
+            print('response timed out')
+            return
+        end
     end
-if computer.uptime() > timeout then
-    print('program was invalid or request timed out.')
+if response() == nil then
+    print('program was invalid.')
     return
 end
 
 local data = response()
-local file = io.open(program:lower(), 'w')
+local file = io.open(string.lower(program), 'w')
 file:write(data)
 file:close()
